625c277d2c923166cbd336d3e3b2d4b8fa733bba
Afochar <aflowchars@gmail.com>
2022-07-15 10:13:56 +0800

    push dist

---
+++ dist/index.aa69868b.js
@@ -0,0 +1,23 @@
+let slideIndex = 1;
+showSlide(slideIndex);
+function plusSlide(sl) {
+    showSlide(slideIndex += sl);
+}
+function currentSlide(sl) {
+    showSlide(slideIndex = sl);
+}
+function showSlide(sl) {
+    let i = 0;
+    let slides = document.getElementsByClassName("tour-photo");
+    let icons = document.getElementsByClassName("icon-slider");
+    console.log(slides);
+    if (sl > slides.length) slideIndex = 1;
+    if (sl < 1) slideIndex = slides.length;
+    for(i = 0; i < icons.length; i++)slides[i].style.display = "none";
+    for(i = 0; i < icons.length; i++)icons[i].className = icons[i].className.replace(" active", "");
+    slides[slideIndex - 1].style.display = "block";
+    icons[slideIndex - 1].className += " active";
+    console.log(slideIndex);
+}
+
+//# sourceMappingURL=index.aa69868b.js.map
